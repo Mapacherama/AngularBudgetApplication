35345784bcff772832ff6bcacd091957f5639174
Mapacherama <jerome_tesselaar@hotmail.com>
2021-09-30 14:34:20 +0200

    A random savefile appears, do you want to fight it? Y / N.

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,24 @@
+import { BudgetItem } from './../../shared/models/budget-item.model';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss']
+})
+export class AddItemFormComponent implements OnInit {
+
+  @Input() item: BudgetItem = new BudgetItem('', null);
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+      this.formSubmit.emit(form.value);
+  }
+
+}
